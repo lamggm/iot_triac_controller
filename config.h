44a5cc22b86071c4eea907a1490bb7fb915d0b31
lamggm <150146192+lamggm@users.noreply.github.com>
2023-11-21 15:25:35 -0300

    Atualizar o config.h
    optimized

--- config.h
+++ config.h
@@ -1,38 +1,13 @@
 /************************ Adafruit IO Config *******************************/
 
-// visit io.adafruit.com if you need to create an account,
-// or if you need your Adafruit IO key.
-#define IO_USERNAME "lamggm"
-#define IO_KEY "aio_aemy54R89qqAQ5L4H178FiJIhxj2"
+#define IO_USERNAME "your user name"
+#define IO_KEY "your key"
 
 /******************************* WIFI **************************************/
 
-// the AdafruitIO_WiFi client will work with the following boards:
-//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
-//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
-//   - Feather HUZZAH ESP32 -> https://www.adafruit.com/product/3405
-//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
-//   - Feather WICED -> https://www.adafruit.com/products/3056
-//   - Adafruit PyPortal -> https://www.adafruit.com/product/4116
-//   - Adafruit Metro M4 Express AirLift Lite ->
-//   https://www.adafruit.com/product/4000
-//   - Adafruit AirLift Breakout -> https://www.adafruit.com/product/4201
-//   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
-//   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
+#define WIFI_SSID "your SSID"
+#define WIFI_PASS "your password"
 
-#define WIFI_SSID "Casa"
-#define WIFI_PASS "Resident.1"
-
-// uncomment the following line if you are using airlift
-// #define USE_AIRLIFT
-
-// uncomment the following line if you are using winc1500
-// #define USE_WINC1500
-
-// uncomment the following line if you are using mrk1010 or nano 33 iot
-//#define ARDUINO_SAMD_MKR1010
-
-// comment out the following lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
 
 #if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
@@ -51,22 +26,3 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
 #else
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 #endif
-/******************************* FONA **************************************/
-
-// the AdafruitIO_FONA client will work with the following boards:
-//   - Feather 32u4 FONA -> https://www.adafruit.com/product/3027
-
-// uncomment the following two lines for 32u4 FONA,
-// and comment out the AdafruitIO_WiFi client in the WIFI section
-// #include "AdafruitIO_FONA.h"
-// AdafruitIO_FONA io(IO_USERNAME, IO_KEY);
-
-/**************************** ETHERNET ************************************/
-
-// the AdafruitIO_Ethernet client will work with the following boards:
-//   - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
-
-// uncomment the following two lines for ethernet,
-// and comment out the AdafruitIO_WiFi client in the WIFI section
-// #include "AdafruitIO_Ethernet.h"
-// AdafruitIO_Ethernet io(IO_USERNAME, IO_KEY);
